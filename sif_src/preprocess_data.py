751d0b0dc48eae1710ab3a3d7995f8ab6db83e39
dj-sorry <yegorduma@gmail.com>
2024-06-05 14:23:01 +0900

    modify preprocessing

--- sif_src/preprocess_data.py
+++ sif_src/preprocess_data.py
@@ -1,12 +1,20 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import pandas as pd
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-def preprocess_text(text):
-    tokens = word_tokenize(text.lower())
+def preprocess_text(text_series):
+
+    text_series = text_series.str.lower()
+    text_series = text_series.apply(word_tokenize)
+
     stop_words = set(stopwords.words('english'))
-    filtered_tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
-    return filtered_tokens
+
+    text_series = text_series.apply(
+        lambda tokens: [word for word in tokens if word.isalnum() and word not in stop_words]
+    )
+    
+    return text_series
