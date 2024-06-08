decdae003b1ea2e70a80226523b770b5e4cde4c3
dj-sorry <yegorduma@gmail.com>
2024-06-08 14:42:49 +0900

    testing not implemented

--- testing/test_sif.py
+++ testing/test_sif.py
@@ -7,23 +7,23 @@ from src.utils import load_word_vectors
 class TestSIF(unittest.TestCase):
     
     def test_load_data(self):
-        # Test data loading
+
         pass
     
     def test_preprocess_text(self):
-        # Test text preprocessing
+
         pass
     
     def test_sif_weights(self):
-        # Test SIF weights computation
+
         pass
     
     def test_sif_embeddings(self):
-        # Test SIF embeddings computation
+
         pass
     
     def test_remove_pc(self):
-        # Test removing principal component
+
         pass
 
 if __name__ == '__main__':
